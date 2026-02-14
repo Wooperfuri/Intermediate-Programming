494ff2d4bdf167ca41167b41d9037b7ef562f806
Nagu <163364588+Wooperfuri@users.noreply.github.com>
2026-02-14 12:09:47 +0800

    Update LA-4.py

--- LA-4.py
+++ LA-4.py
@@ -2,19 +2,17 @@
 
 age = int(input("Enter your age: "))
 
-# Stop program if underage
 if age <= 17:
     print("Oops invalid age.")
-    exit()
-
-# Ask ID only if age is valid
-has_id_input = input("Do you have a valid ID? (yes/no): ").lower()
-has_id = True if has_id_input == "yes" else False
-
-# Eligibility check
-if age >= 60 and has_id:
-    print("Eligible! You also qualify for senior discount.")
-elif age >= 18 and has_id:
-    print("Eligible!")
 else:
-    print("Not eligible.")
+    # Ask ID only if age is valid
+    has_id_input = input("Do you have a valid ID? (yes/no): ").lower()
+    has_id = True if has_id_input == "yes" else False
+
+    # Eligibility check
+    if age >= 60 and has_id:
+        print("Eligible! You also qualify for senior discount.")
+    elif age >= 18 and has_id:
+        print("Eligible!")
+    else:
+        print("Not eligible.")
