2121422c94005fd8e3b8e9d6aa3afce564afa95b
Nagu <163364588+Wooperfuri@users.noreply.github.com>
2026-02-21 07:48:31 +0800

    Update LA-4.py

--- LA-4.py
+++ LA-4.py
@@ -1,18 +1,19 @@
 # Eligibility Checker Program
 
 age = int(input("Enter your age: "))
+has_id = False 
 
-if age <= 17:
-    print("Oops invalid age.")
-else:
-    # Ask ID only if age is valid
+if age >= 18:
     has_id_input = input("Do you have a valid ID? (yes/no): ").lower()
-    has_id = True if has_id_input == "yes" else False
+    
+    if has_id_input == "yes":
+        has_id = True
 
-    # Eligibility check
-    if age >= 60 and has_id:
-        print("Eligible! You also qualify for senior discount.")
-    elif age >= 18 and has_id:
-        print("Eligible!")
-    else:
-        print("Not eligible.")
+if age < 18:
+    print("Not eligible.")
+elif age >= 60 and has_id:
+    print("Eligible for senior discount.")
+elif age >= 18 and has_id:
+    print("Eligible!")
+else:
+    print("Not eligible. Valid ID required.")
